6759ce7505f3edce081eb4a20d388c1eb489b756
mallium <mallium@gmail.com>
2015-07-31 16:16:49 -0400

    add the fileurl as an optional parameter

--- src/GetReadingInfo.java
+++ src/GetReadingInfo.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -37,14 +38,17 @@ public class GetReadingInfo extends HttpServlet {
 		String readingId = request.getParameter("readingid");  
 		String usr = request.getParameter("usr");  
 		String callback = request.getParameter("callback"); 
-	
+		String fileURL = request.getParameter("fileurl"); 
+		
+		db = new ReadingDBInterface(cm.dbstring,cm.dbuser,cm.dbpass);
+		db.openConnection();
+		
 		if(readingId != null && readingId.length()>0){
-			db = new ReadingDBInterface(cm.dbstring,cm.dbuser,cm.dbpass);
-			db.openConnection();
+			
 			
 			Reading r = db.getRedingInfo(readingId);
 			db.getReadingQuestions(r,usr);
-			db.closeConnection();
+			
 			
 			if(r != null){
 				//System.out.println(r.getTitle());
@@ -59,7 +63,31 @@ public class GetReadingInfo extends HttpServlet {
 			}else{
 				System.out.println("{error:\"no reading found\"}");
 			}
+		}else if(fileURL != null && fileURL.length()>0){
+			ArrayList<String> readingIds = db.getAllRadingForAPage("", 0, fileURL, "html"); // TODO manage file formats!!!
+			if(readingIds.size()>0){
+				Reading r = db.getRedingInfo(readingIds.get(0));
+				db.getReadingQuestions(r,usr);
+				
+				
+				if(r != null){
+					//System.out.println(r.getTitle());
+					if(callback != null && callback.length() > 0){
+						out.write(callback+"(");
+						out.write(r.jsonFormat());
+						out.write(")");
+					}else{
+						out.write(r.jsonFormat());
+					}
+					
+				}else{
+					System.out.println("{error:\"no reading found\"}");
+				}
+			}
+				
 		}
+		
+		db.closeConnection();
 	}
 
 	/**
